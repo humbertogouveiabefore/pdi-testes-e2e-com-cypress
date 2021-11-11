119519e6918452ed710a078759610dc8062c1340
humbertogouveiabefore <humberto.gouveia@before.com.br>
2021-11-11 20:20:14 -0300

    Skip signup tes

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,5 +1,5 @@
 
-it('successfully signs up using confirmation code sent via email', () => {
+it.skip('successfully signs up using confirmation code sent via email', () => {
   const faker = require('faker')
   const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
   const password = Cypress.env('USER_PASSWORD')
